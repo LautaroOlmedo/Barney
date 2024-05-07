091bacbee01e82fe135d5e18d4d15e30c44b70db
Santino <santinopiantini@gmail.com>
2024-05-07 11:22:41 -0300

    Add html and css in LoginPage 2

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -4,7 +4,50 @@ export const LoginPage = () => {
   return (
     <div>
       <p>LAUTI PUTO</p>
-      <h1>LOGIN</h1>
     </div>
   );
 };
+
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="initial-scale=1, width=device-width" />
+
+    <link rel="stylesheet" href="./global.css" />
+    <link rel="stylesheet" href="./index.css" />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=Poppins:wght@500;600&display=swap"
+    />
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css2?family=ES Peak TRIAL:wght@500&display=swap"
+    />
+  </head>
+  <body>
+    <div class="framelogin">
+      <section class="frame-wrapper">
+        <div class="frame">
+          <input class="frame-child" id="contraseñaInput" type="text" />
+
+          <button class="ingresar-wrapper" id="btnLogin">
+            <div class="ingresar">Ingresar</div>
+          </button>
+          <button class="btnolvidocontra" id="contraseñaOlvido">
+            ¿Olvidaste tu contraseña?
+          </button>
+          <div class="frame1">
+            <img
+              class="sin-ttulo-2marron-1"
+              alt=""
+              src="./public/sin-ttulo2marron-1@2x.png"
+            />
+
+            <img class="tinder-1-icon" alt="" src="./public/tinder-1@2x.png" />
+          </div>
+          <input class="frame-item" id="emailImput" type="text" />
+        </div>
+      </section>
+    </div>
+  </body>
+</html>
