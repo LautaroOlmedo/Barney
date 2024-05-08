af71c8ab6457954bc6d26ee3d0c2540dd50bc40b
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 <lautaroolmedo77@gmail.com>
2024-05-08 16:56:37 -0300

    DogCart. First version

--- src/components/Carts/Dog/DogCart.tsx
+++ src/components/Carts/Dog/DogCart.tsx
@@ -1,3 +1,16 @@
-export const DogCart = () => {
-  return <div>HOLA</div>;
+import { Dog } from "../../../types/Dog";
+import { Customer } from "../../../types/Customer";
+
+export const DogCart: React.FC<{ dog: Dog; customer: Customer }> = ({
+  dog,
+  customer,
+}) => {
+  return (
+    <div>
+      <p>name: {dog.Name != null ? dog.Name : "no hay datos para mostrar"}</p>
+      <p>age: {dog.Age}</p>
+      <p>description: {dog.Description}</p>
+      <p>customer: {customer.User.Person.Name}</p>
+    </div>
+  );
 };
