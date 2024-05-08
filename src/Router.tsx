070545d1c14c4289df2f3129caf157b79f5c462d
Gonzalo <gonzalogallardo216@gmail.com>
2024-05-08 15:51:01 -0300

    Cambio de nombre de archivos en carpeta profile

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,7 +6,7 @@ import { Route, Routes } from "react-router-dom";
 import { HomePage } from "./pages/home";
 import { LoginPage } from "./pages/login";
 import { RouterLayout } from "./common/RouterLayout";
-import { ProfilePage } from "./pages/profile/profile";
+import { UserProfilePage } from "./pages/profile/userProfile";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
@@ -14,7 +14,7 @@ export const AppRouter: React.FC<{}> = () => {
       <Route path="/" element={<RouterLayout />}>
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={<UserProfilePage />} />
         // si la ruta no tiene que tener navBar o footer va por acá bajo
       </Route>
     </Routes>
