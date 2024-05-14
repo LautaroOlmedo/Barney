a38586f9e28ee1194f02edc38c30de2437a113f4
barney <tinodeclascaballer@gmail.com>
2024-05-14 12:23:33 -0300

    created DogProfileSelect.css

--- src/pages/DogProfileSelect/DogProfileSelect.tsx
+++ src/pages/DogProfileSelect/DogProfileSelect.tsx
@@ -4,10 +4,16 @@ import React from "react";
 
 export const DogProfileSelect: React.FC <{}> = () => {
 return(
-    <div>
-        <h1>Perfil</h1>
-        <button>Yo</button>
-        <button>Perros</button>
+    <div className="div">
+        <div className="divPerros">
+            <h1>Perfil</h1>    
+        </div>
+        <div className="divButtonYo">
+            <button>Yo</button>
+        </div>
+        <div className="divButtonPerros">
+            <button>Perros</button>
+        </div>
     </div>
 );
 
