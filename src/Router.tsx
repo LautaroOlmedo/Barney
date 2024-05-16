2527e34d5e747bde1ad2b1a0537fc1766823c55c
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 <lautaroolmedo77@gmail.com>
2024-05-16 13:24:23 -0300

    added route for dogProfile

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ import { UserProfilePage } from "./pages/UserProfile/userProfile";
 import { DogProfileSelect } from "./pages/DogProfileSelect/DogProfileSelect";
 import { data } from "./mocks/userMockData";
 import { dogData } from "./mocks/dogMockData";
+import { DogProfilePage } from "./pages/dogProfile/DogProfile";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
@@ -17,7 +18,14 @@ export const AppRouter: React.FC<{}> = () => {
       <Route path="/" element={<RouterLayout />}>
         <Route path="/home" element={<HomePage dog={dogData[1]} />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<UserProfilePage user={data[0]} />} />
+        <Route
+          path="/userProfile"
+          element={<UserProfilePage user={data[0]} />}
+        />
+        <Route
+          path="/dogProfile"
+          element={<DogProfilePage dog={dogData[0]} user={data[0]} />}
+        />
         <Route
           path="/dogProfileSelect"
           element={<DogProfileSelect dog={dogData[0]} />}
