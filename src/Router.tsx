174149c535062621ccbcfc520d9d0a6d6ee9b57b
Santino <santinopiantini@gmail.com>
2024-05-16 00:03:49 -0300

    add login error alert

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
-// -------------------- -------------------- -------------------- -------------------- --------------------
 
 import { HomePage } from "./pages/home/Home";
 import { LoginPage } from "./pages/login";
 import { RouterLayout } from "./common/routerLayout/RouterLayout";
 import { UserProfilePage } from "./pages/UserProfile/userProfile";
 import { LoginError } from "./pages/login/errors/loginError";
+
 
 export const AppRouter: React.FC<{}> = () => {
   return (
@@ -16,7 +16,7 @@ export const AppRouter: React.FC<{}> = () => {
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/profile" element={<UserProfilePage />} />
-        <Route path="/login/errors" element={<LoginError error="Usuario o contraseña no encontrados"/>} />
+        <Route path="/login/errors" element={<LoginError error="Usuario o contraseña incorrectos"/>} />
         // si la ruta no tiene que tener navBar o footer va por acá bajo
       </Route>
     </Routes>
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,45 @@
-import React from "react";
+import React, { useState, FormEvent } from "react";
 import "./login.css";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+/*import users from "./personMockData.json"; 
+
+type User = {
+  Person: {
+    ID: string;
+    Name: string;
+    Lastname: string;
+    age: number;
+    DNI: string;
+  };
+  Username: string;
+  Password: string;
+  emails: string[];
+  phones: string[];
+};*/
+
 export const LoginPage = () => {
+  /*
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = users.find((user: User) => user.emails.includes(email));
+    if (user && user.Password === password) {
+      navigate("/home");  // Navegar a la página de inicio en caso de éxito
+    } else {
+      setError("El usuario o la contraseña no son correctos."); // Mostrar error si las credenciales no coinciden
+    }
+  };
+  */
   return (
     <div>
        <div className="framelogin">
         <section className="frame-wrapper">
           <div className="frame">
-            <input className="frame-child" id="contraseñaInput" type="password" placeholder="Contraseña"/>
+            <input className="frame-child" id="contraseñaInput" type="password" placeholder="Contraseña" /*value={password} onChange={(e) => setPassword(e.target.value)}*//>
             <button className="ingresar-wrapper" id="btnLogin">
                <div className="ingresar">Ingresar</div>
              </button>
@@ -17,12 +49,10 @@ export const LoginPage = () => {
              <div className="frame1">
                <img className="tinder-1-icon" alt="" src="./img/barneyLogos/namePawBrown.png" />
              </div>
-             <input className="frame-item" id="emailImput" type="text" placeholder="Email" />
+             <input className="frame-item" id="emailImput" type="text" placeholder="Email" /*value={email} onChange={(e) => setEmail(e.target.value)}*//>
           </div>
          </section>
         </div>
     </div>
-
-    
   );
 };
