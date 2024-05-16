dd2bcfa3246fb717a9e48ce51eed5ae6dd7e48a8
barney <tinodeclascaballer@gmail.com>
2024-05-16 02:34:16 -0300

    Fixed bugs in router.tsx, finished DogProfileSelect.ts and DogCartPresentation.ts components and created dogMockData.ts file in mocks folder

--- src/components/Carts/Dog/DogCartPresentation/DogCartPresentation.tsx
+++ src/components/Carts/Dog/DogCartPresentation/DogCartPresentation.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Dog } from "../../../../types/Dog";
+import "./DogCartPresentation.css";
 
 
 
-export const DogCartPresentation: React.FC<{dogs: Dog}> = ({dogs}) => {
+export const DogCartPresentation: React.FC<{dog: Dog}> = ({dog}) => {
     return(
-        <div>
-            <div>
-                <img src={dogs.images[0]} alt="foto perfil cucu" />
+        <div className="dogProfile">
+            <div className="dogImg">
+                <img src={dog.images[0]} alt="foto perfil cucu" />
             </div>
-            <p>{dogs.Name}</p>
-            <button> Perfil </button>
-            <button> Matches </button>
+            <div className="dogName">
+                <p>{dog.Name}</p>
+            </div>
         </div>
     )
 }
--- src/pages/DogProfileSelect/DogProfileSelect.tsx
+++ src/pages/DogProfileSelect/DogProfileSelect.tsx
@@ -1,18 +1,16 @@
 import React from "react";
+import { DogCartPresentation } from "../../components/Carts/Dog/DogCartPresentation/DogCartPresentation";
+import { Dog } from "../../types/Dog";
 import "./DogProfileSelect.css";
 
-
-export const DogProfileSelect: React.FC <{}> = () => {
+export const DogProfileSelect: React.FC <{dog:Dog}> = ({ dog }) => {
 return(
     <div className="div">
         <div className="divPerros">
-            <h1>Perfil</h1>    
+            <h1>Perros</h1>    
         </div>
-        <div className="divButtonYo">
-            <button>Yo</button>
-        </div>
-        <div className="divButtonPerros">
-            <button>Perros</button>
+        <div>
+            <DogCartPresentation dog={dog} />
         </div>
     </div>
 );
