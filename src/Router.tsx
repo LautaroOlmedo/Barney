f99faaa2a0f3947085f0819942124200c8612ba8
Gonzalo <gonzalogallardo216@gmail.com>
2024-05-15 23:51:26 -0300

    Cambien todo el userProfile , cree el userMockData dentro de mocks y agregue el trabajo de Tomi de UserCard tsx y css

--- src/Router.tsx
+++ src/Router.tsx
@@ -7,14 +7,17 @@ import { HomePage } from "./pages/home/Home";
 import { LoginPage } from "./pages/login";
 import { RouterLayout } from "./common/routerLayout/RouterLayout";
 import { UserProfilePage } from "./pages/UserProfile/userProfile";
+import {data} from "./mocks/userMockData"
+
 
 export const AppRouter: React.FC<{}> = () => {
+
   return (
     <Routes>
       <Route path="/" element={<RouterLayout />}>
         <Route path="/home" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="/profile" element={<UserProfilePage />} />
+        <Route path="/profile" element={<UserProfilePage user={data[0]}/>} />
         // si la ruta no tiene que tener navBar o footer va por acá bajo
       </Route>
     </Routes>
