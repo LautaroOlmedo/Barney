668eb0a72da9489533ee754997ef1d36b3461b69
Tomas <jtomaspeiretti@gmail.com>
2024-05-30 15:40:47 -0300

    Navbar responsive terminado

--- src/common/navBar/NavBar.tsx
+++ src/common/navBar/NavBar.tsx
@@ -5,44 +5,40 @@ import "./NavBar.css"
 
 export const NavBar: React.FC<{}> = ({}) => {
   return (
-    <nav className="navbar">
-      <div className="navbar-logo">
-        <Link to={"/home"}>
-          <img src="./img/barneyLogos/pawFlameBrown.png" alt="" />
-        </Link>
-      </div>
+    <nav >
+      <input id="menu__toggle" type="checkbox" />
+      <label className="menu__btn" htmlFor="menu__toggle">
+          <span></span>
+      </label>
 
-      <div>
-        <Link to={"/home"} className="navbar-link">
-          Inicio
-        </Link>
-      </div>
-      <div>
-        <Link to={"/userProfile" } className="navbar-link">
-          Mi Perfil
-         </Link>
-      </div>
-      <div>
-        <Link to={"/dogProfile"} className="navbar-link">
-          Mi Perro
-        </Link>
-      </div>
-
-      {/* <ul>
-        <li className="dropdown">
-          <a href="" className="dropbtn">Perfiles</a>
-          <div className="dropdown-content">
-            <a href="">Link 1</a>
-            <a href="">Link 2</a>
-
+      <div className="navbar">
+        <div className="navbar-logo">
+          <Link to={"/home"}>
+            <img src="./img/barneyLogos/pawFlameBrown.png" alt="" />
+          </Link>
+        </div>
+          <div>
+            <Link to={"/home"} className="navbar-link">
+              Inicio
+            </Link>
           </div>
-        </li>
-      </ul> */}
-      <div>
-        <button className="navbar-button">
-          Cerrar Sesion
-        </button>
-      </div>    
+          <div>
+            <Link to={"/userProfile" } className="navbar-link">
+              Mi Perfil
+            </Link>
+          </div>
+          <div>
+            <Link to={"/dogProfile"} className="navbar-link">
+              Mi Perro
+            </Link>
+          </div>
+          <div>
+            <button className="navbar-button">
+              Cerrar Sesion
+            </button>
+          </div>
+      </div>  
+
     </nav>
   );
 };
