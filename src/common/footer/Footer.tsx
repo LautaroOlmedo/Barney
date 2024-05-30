e04c2634366ffd07c219e48c2b763b5c8c8971e6
Tomas <jtomaspeiretti@gmail.com>
2024-05-30 11:47:00 -0300

    Navbar y Footer

--- src/common/footer/Footer.tsx
+++ src/common/footer/Footer.tsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Footer.css"
 
 export const Footer = () => {
-  return <div></div>;
+  return (
+    <footer>
+      <div className="footer">        
+        <div className="content">
+          <ul>
+            <li><a href="#">Contact us</a></li>
+            <li><a href="#">Our Services</a></li>
+            <li><a href="#">Privacy Policy</a></li>
+            <li><a href="#">Terms & Conditions</a></li>
+          </ul>
+        </div>
+    </div>
+    </footer>
+  )
+  
 };
