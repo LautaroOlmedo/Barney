db90ff7c074ac4f269d26d67955b65d82296f86d
Santino <santinopiantini@gmail.com>
2024-09-28 02:36:49 -0300

    Registration page was created with its finished CSS, and conect with login page

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,6 +10,7 @@ import { data } from "./mocks/userMockData";
 import { dogData } from "./mocks/dogMockData";
 import { DogProfilePage } from "./pages/dogProfile/DogProfile";
 import { DogUpdatePage } from "./pages/dogProfile/dogUpdatePage";
+import { RegisterPage } from "./pages/register/Register";
 
 export const AppRouter: React.FC<{}> = () => {
   return (
@@ -20,6 +21,7 @@ export const AppRouter: React.FC<{}> = () => {
           element={<HomePage user={data[0]} dog={dogData[1]} />}
         />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/userProfile" element={<UserProfilePage />} />
         <Route path="/dogProfile" element={<DogProfilePage />} />
         <Route
---
+++ src/pages/register/Register.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import"./register.css";
+
+export const RegisterPage = () => {
+
+    return (
+      <div>
+        <div className="frameregister">
+          <section className="registerwrapper">
+            <form className="registerCamp">
+                <div className="framelogo">
+                    <img
+                    className="logo"
+                    alt=""
+                    src="./img/barneyLogos/namePawBrown.png" />
+                </div>
+              <input
+                name="firstName"
+                className="firstName"
+                id="firstName"
+                type="text"
+                placeholder="Nombre"
+                required
+                autoComplete="off"
+              />
+              <input
+                name="lastName"
+                className="lastName"
+                id="lastName"
+                type="text"
+                placeholder="Apellido"
+                required
+                autoComplete="off"
+              />
+              <input
+                name="dni"
+                className="dni"
+                id="dni"
+                type="text"
+                placeholder="DNI"
+                required
+                autoComplete="off"
+              />
+              <input
+                name="birthDate"
+                className="birthDate"
+                id="birthDate"
+                type="date"
+                placeholder="Fecha de Nacimiento"
+                required
+                autoComplete="off"
+              />
+              <input
+                name="email"
+                className="email"
+                id="email"
+                type="email"
+                placeholder="Email"
+                required
+                autoComplete="off"
+              />
+              <input
+                name="confirmEmail"
+                className="confirmEmail"
+                id="confirmEmail"
+                type="email"
+                placeholder="Confirmar Email"
+                required
+                autoComplete="off"
+              />
+              <input
+                className="password"
+                name="password"
+                id="password"
+                type="password"
+                placeholder="Contraseña"
+                required
+                autoComplete="off"
+              />
+              <input
+                className="confirmPassword"
+                name="confirmPassword"
+                id="confirmPassword"
+                type="password"
+                placeholder="Confirmar Contraseña"
+                required
+                autoComplete="off"
+              />
+              
+              <button type="submit" className="btnRegister" id="btnRegister">
+                 <div className="ingresar">Registrar</div>
+              </button>
+
+
+            </form>
+          </section>
+        </div>
+      </div>
+    );
+  };
