14020d3947504688f94072eb696911f4401611f2
Santino <santinopiantini@gmail.com>
2024-09-28 19:14:18 -0300

    register validations finish

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,85 +1,23 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import  useForm  from "./hooks/useForm";
 import"./register.css";
 
 export const RegisterPage = () => {
     const navigate = useNavigate(); // Usar useNavigate para la navegación
+    
+    const { formData, errors, setErrors, handleChange, validateForm } = useForm();
 
-    const [formData, setFormData] = useState({
-      firstName: "",
-      lastName: "",
-      dni: "",
-      birthDate: "",
-      email: "",
-      phoneNumber: "",
-      password: "",
-      confirmPassword: "",
-    });
-
-    const [errors, setErrors] = useState({});
-
-    const handleRegister = (e) => {
+    const handleRegister = (e: React.FormEvent) => {
       e.preventDefault();
       const validationErrors = validateForm();
       if (Object.keys(validationErrors).length === 0) {
         // Si no hay errores, redirigir al login
         navigate("/login");
       } else {
+        // Establecer errores en el estado
         setErrors(validationErrors);
       }
-    };
-  
-    const validateForm = () => {
-      const errors = {};
-      // Validar si todos los campos están completos
-      if (!formData.firstName) errors.firstName = "El campo 'Nombre' es obligatorio.";
-      if (!formData.lastName) errors.lastName = "El campo 'Apellido' es obligatorio.";
-      if (!formData.dni) errors.dni = "El campo 'DNI' es obligatorio.";
-      if (!formData.birthDate) errors.birthDate = "El campo 'Fecha de Nacimiento' es obligatorio."; 
-      if (!formData.email) errors.email = "El campo 'Email' es obligatorio.";
-      if (!formData.phoneNumber) errors.phoneNumber = "El campo 'Teléfono' es obligatorio.";
-      if (!formData.password) errors.password = "El campo 'Contraseña' es obligatorio.";
-      if (!formData.confirmPassword) errors.confirmPassword = "El campo 'Confirmar Contraseña' es obligatorio.";
-
-    
-      // Validar nombre (una palabra, sin números ni espacios)
-      if (!/^[A-Za-z]+$/.test(formData.firstName)) {
-        errors.firstName = "Ingrese un nombre valido.";
-      }
-  
-      // Validar apellido (solo letras)
-      if (!/^[A-Za-zÀ-ÿ\s]+$/.test(formData.lastName)) {
-        errors.lastName = "Ingrese un Apellido valido.";
-      }
-  
-      // Validar DNI (solo números sin espacios ni puntos)
-      if (!/^\d+$/.test(formData.dni)) {
-        errors.dni = "El DNI debe contener solo números sin espacios ni puntos.";
-      }
-  
-      // Validar email
-      if (!/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(formData.email)) {
-        errors.email = "El email no es válido.";
-      }
-  
-      // Validar teléfono (solo números sin espacios ni símbolos)
-      if (!/^\d+$/.test(formData.phoneNumber)) {
-        errors.phoneNumber = "El número de teléfono debe contener solo números.";
-      }
-  
-      // Validar que las contraseñas coincidan
-      if (formData.password !== formData.confirmPassword) {
-        errors.password = "Las contraseñas no coinciden.";
-      }
-  
-      return errors;
-    };
-  
-    const handleChange = (e) => {
-      setFormData({
-        ...formData,
-        [e.target.name]: e.target.value,
-      });
     };
     
     return (
