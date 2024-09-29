cf73c2102148a2fb68628b136c20b0c15ceac3d6
lautaroolmedo <lolmedo@arquant.com.ar>
2024-09-29 19:10:25 -0300

    seccond

--- src/components/Matchs/Matchs.tsx
+++ src/components/Matchs/Matchs.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import { Dog } from "../../types/Dog";
 import "./matchs.css";
 
-export const Matchs: React.FC<{ dog: Dog }> = ({ dog }) => {
+export const Matchs: React.FC<{ dogs: Dog[] }> = ({ dogs }) => {
   return (
-    <div className="container">
-      <p>MATCHES</p>
-    </div>
+    <div></div>
   );
 };
