1dc2307600c6b5e1cbbb8e0ed412ac6f267217c6
barney <tinodeclascaballer@gmail.com>
2024-10-14 17:17:18 -0300

    Resolved problems

--- client/src/common/footer/Footer.tsx
+++ client/src/common/footer/Footer.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Footer.css"
-import EmergentFooter from "../../components/EmergentFooter/EmergentFooter"; 
+import EmergentFooter from "../../components/EmergentFooter/emergentFooter"; 
 
 
 export const Footer = () => {
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import  useForm  from "./hooks/useForm";
+import  useForm  from "./hooks/useForms";
 import"./register.css";
 
 export const RegisterPage = () => {
