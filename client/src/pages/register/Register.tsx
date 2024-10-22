3f47b08b8b9373ab34e57c3f0968f068040f2111
barney <tinodeclascaballer@gmail.com>
2024-10-22 18:09:19 -0300

    Modified problems in Register and useForm .tsx

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import useForm from "../register/hooks/useForm";
 import"./register.css";
 
 export const RegisterPage = () => {
