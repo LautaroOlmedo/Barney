dd09cbda6969619d0ed67517d0e458758f04e8fe
Gonzalo <gonzalogallardo216@gmail.com>
2024-11-20 14:58:22 -0300

    cambio del mockdata dog

--- server/src/infraestructure/dependencies.ts
+++ server/src/infraestructure/dependencies.ts
@@ -2,4 +2,3 @@ import { UserController } from "./user-controller";
 import { ProductController } from "./product-controller";
 export const userController = new UserController();
 export const productController = new ProductController();
->>>>>>>>> Temporary merge branch 2
